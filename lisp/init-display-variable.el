f57328b5f122639f71633fe78a9619fdc7fa9d2f
xcv58 <chenyihonglove@gmail.com>
2013-12-24 00:42:26 -0500

    add smart-mode-line

--- lisp/init-display-variable.el
+++ lisp/init-display-variable.el
@@ -77,4 +77,34 @@
               (concat ";; Emacs is ready for you: " (or user-login-name "") "!\n\n"))
               ;(concat ";; Happy hacking " (or user-login-name "") "!\n\n"))
 
+;;; Customize status bar
+(require-package 'smart-mode-line)
+(require 'smart-mode-line)
+(setq sml/theme 'dark)
+
+(setq sml/shorten-directory t)
+(setq sml/shorten-modes t)
+(setq sml/name-width 25)
+(setq sml/mode-width 'full)
+
+(add-to-list 'sml/hidden-modes " AC")
+(add-to-list 'sml/hidden-modes " Anzu")
+(add-to-list 'sml/hidden-modes " Golden")
+(add-to-list 'sml/hidden-modes " Undo-Tree")
+(add-to-list 'sml/replacer-regexp-list '("^~.*/Work/" ":Work:"))
+; (add-to-list 'sml/hidden-modes " Gtags")
+; (add-to-list 'sml/hidden-modes " Abbrev")
+; (add-to-list 'sml/hidden-modes " Fill")
+; (add-to-list 'sml/hidden-modes " Guide")
+(sml/setup)
+
+
+;;----------------------------------------------------------------------------
+;; Enable uniquify buffer name
+;;----------------------------------------------------------------------------
+(require 'uniquify) 
+(setq 
+  uniquify-buffer-name-style 'post-forward
+  uniquify-separator ":")
+
 (provide 'init-display-variable)
