c42daf8248f5b16f568284e0e33bbbcadf199a07
xcv58 <chenyihonglove@gmail.com>
2013-12-25 03:08:33 -0500

    change exec-path

--- init.el
+++ init.el
@@ -7,14 +7,17 @@
 (defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
 (defconst *is-a-mac* (eq system-type 'darwin))
 
+(setenv "PATH" (concat "/usr/local/bin" (getenv "PATH")))
+    (setq exec-path (append exec-path '("/usr/local/bin")))
+
 ;;----------------------------------------------------------------------------
 ;; Path for eshell
 ;;----------------------------------------------------------------------------
-(defun eshell-mode-hook-func ()
-  (setq eshell-path-env (concat "/usr/local/bin:" eshell-path-env))
-  (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
-  (define-key eshell-mode-map (kbd "M-s") 'other-window-or-split))
-(add-hook 'eshell-mode-hook 'eshell-mode-hook-func)
+;; (defun eshell-mode-hook-func ()
+;;   (setq eshell-path-env (concat "/usr/local/bin:" eshell-path-env))
+;;   (setenv "PATH" (concat "/usr/local/bin:" (getenv "PATH")))
+;;   (define-key eshell-mode-map (kbd "M-s") 'other-window-or-split))
+;; (add-hook 'eshell-mode-hook 'eshell-mode-hook-func)
 
 ;----------------------------------------------------------------------------
 ; Bootstrap config
