5a8741c71ba591f7c25df76032c65bd2fbec1bef
xcv58 <chenyihonglove@gmail.com>
2014-01-11 15:59:02 -0500

    change the indent for ns-frame.sh from \t to space

--- ns-frame.sh
+++ ns-frame.sh
@@ -1,13 +1,13 @@
 existNsFrame=$(/usr/local/bin/emacsclient -e "(exist-ns-frame)")
 if [ $# -eq 0 ]
 then
-	daemon=false
+    daemon=false
 else
-	daemon=true
+    daemon=true
 fi
 if [ "$existNsFrame" == "" ]; then
-	/usr/local/bin/emacs --daemon
+    /usr/local/bin/emacs --daemon
 fi
 if [ "$existNsFrame" != "ns" ] && [ "$daemon" != "true" ]; then
-	/usr/local/bin/emacsclient -c -n
+    /usr/local/bin/emacsclient -c -n
 fi
