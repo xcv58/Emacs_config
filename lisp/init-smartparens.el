b5774d82345d8562e13df41bdfa0ef958b750f38
xcv58 <chenyihonglove@gmail.com>
2014-02-13 15:33:27 -0500

    add keybind for using smartparents in evil

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -1,5 +1,11 @@
 (require-package 'smartparens)
 (require 'smartparens-config)
+
 (smartparens-global-mode t)
+(sp-pair "(" ")" :wrap "C-(")
+(sp-pair "(" ")" :wrap "C-)")
+(sp-pair "{" "}" :wrap "C-{")
+(sp-pair "{" "}" :wrap "C-}")
+(sp-pair "\"" "\"" :wrap "C-\"")
 
 (provide 'init-smartparens)
