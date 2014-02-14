6d5f0e157620faf791f436865a5f7b1c96751f6c
xcv58 <chenyihonglove@gmail.com>
2014-02-13 23:29:26 -0500

    add key bind for sp-up/backward/forward-sexp

--- lisp/init-smartparens.el
+++ lisp/init-smartparens.el
@@ -9,4 +9,9 @@
 (sp-pair "{" "}" :wrap "C-}")
 (sp-pair "\"" "\"" :wrap "C-\"")
 
+(define-key sp-keymap (kbd "C-c ,") 'sp-up-sexp)
+(define-key sp-keymap (kbd "M-e") 'sp-up-sexp)
+(define-key sp-keymap (kbd "M-b") 'sp-backward-sexp)
+(define-key sp-keymap (kbd "M-f") 'sp-forward-sexp)
+
 (provide 'init-smartparens)
