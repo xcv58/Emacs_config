d3c51225ccb1562a4a7f224c0bc3a342a92bd414
xcv58 <chenyihonglove@gmail.com>
2014-02-16 20:38:49 -0500

    add smart-ascope-init by find .gitignore to detect directory of project

--- lisp/init-ascope.el
+++ lisp/init-ascope.el
@@ -3,4 +3,14 @@
 
 (add-hook 'ascope-list-entry-hook 'turn-off-evil-mode)
 
+(defun smart-ascope-init ()
+  (interactive)
+  (let ((project-directory
+	 (progn
+	   (if (buffer-file-name)
+	       (catch 'found-it (find-file-r (buffer-file-name) ".gitignore"))
+	     (error "buffer is not visiting a file")))))
+    (if project-directory
+	(ascope-init (file-name-directory project-directory)))))
+
 (provide 'init-ascope)
