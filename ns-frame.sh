fbc35cfb59f84b956c63f98caf5c31f9e30678a8
xcv58 <chenyihonglove@gmail.com>
2014-02-23 22:05:33 -0500

    change ns-frame.sh to enable auto active and toggle emacs.

--- ns-frame.sh
+++ ns-frame.sh
@@ -1,15 +1,32 @@
 existNsFrame=$(/usr/local/bin/emacsclient -e "(exist-ns-frame)")
-if [ $# -eq 0 ]
-then
-    daemon=false
-else
-    daemon=true
-fi
 if [ "$existNsFrame" == "" ]
 then
     /usr/local/bin/emacs --daemon
 fi
 if [ "$existNsFrame" != "ns" ] && [ "$daemon" != "true" ]
 then
-    /usr/local/bin/emacsclient -c -n
+    /usr/local/bin/emacsclient -c -n $*
+    osascript <<FRONTMOST
+set appName to "Emacs"
+set appID to bundle identifier of (info for (path to application appName))
+tell application "System Events"
+    set frontmost of process appName to true
+end tell
+FRONTMOST
+else
+    osascript <<TOGGLEVISIBLE
+set appName to "Emacs"
+set appID to bundle identifier of (info for (path to application appName))
+tell application "System Events"
+    if not (exists process appName) then
+        tell application appID to activate
+    else
+        if frontmost of process appName then
+            set visible of process appName to false
+        else
+            set frontmost of process appName to true
+        end if
+    end if
+end tell
+TOGGLEVISIBLE
 fi
