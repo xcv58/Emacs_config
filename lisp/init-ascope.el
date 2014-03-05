27132e132b2cbcbca1942d30a2fa704cadd2c0d0
xcv58 <chenyihonglove@gmail.com>
2014-03-04 23:04:40 -0500

    fix indent.

--- lisp/init-ascope.el
+++ lisp/init-ascope.el
@@ -6,11 +6,11 @@
 (defun smart-ascope-init ()
   (interactive)
   (let ((project-directory
-	 (progn
-	   (if (buffer-file-name)
-	       (catch 'found-it (find-file-r (buffer-file-name) ".gitignore"))
-	     (error "buffer is not visiting a file")))))
+         (progn
+           (if (buffer-file-name)
+               (catch 'found-it (find-file-r (buffer-file-name) ".gitignore"))
+             (error "buffer is not visiting a file")))))
     (if project-directory
-	(ascope-init (file-name-directory project-directory)))))
+        (ascope-init (file-name-directory project-directory)))))
 
 (provide 'init-ascope)
