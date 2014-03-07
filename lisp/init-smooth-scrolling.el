e60dd0c506622bf57f24c5cf417659099316ffd0
xcv58 <chenyihonglove@gmail.com>
2014-03-06 19:37:32 -0500

    change smooth scrolling parameter

--- lisp/init-smooth-scrolling.el
+++ lisp/init-smooth-scrolling.el
@@ -4,16 +4,20 @@
 
 (sublimity-global-mode)
 
-(setq sublimity-scroll-weight 10
-      sublimity-scroll-drift-length 5)
-(setq sublimity-map-size 28)
+(setq sublimity-scroll-weight 8
+      sublimity-scroll-drift-length 1)
+(setq sublimity-map-size 24)
 (setq sublimity-map-fraction 0.5)
 (setq sublimity-map-on-scroll t)
 
-(sublimity-map-set-delay 10)
+(sublimity-map-set-delay 32)
 
 (add-hook 'sublimity-map-setup-hook
           (lambda ()
             (setq mode-line-format nil)))
+
+(require-package 'smooth-scrolling)
+(setq smooth-scroll-margin 4)
+
 
 (provide 'init-smooth-scrolling)
