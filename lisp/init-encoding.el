0ed08299f677b5d8cb1b8a1658cd12cfc3991c3d
xcv58 <chenyihonglove@gmail.com>
2014-04-02 02:44:51 -0400

    add revert-buffer-no-confirm.

--- lisp/init-encoding.el
+++ lisp/init-encoding.el
@@ -13,9 +13,16 @@
 (defun revert-buffer-with-coding-system-no-confirm (coding-system)
   "Call `revert-buffer-with-coding-system', but when `revert-buffer' do not need confirm."
   (interactive "zCoding system for visited file (default nil): ")
-  (let ((coding-system-for-read coding-system))
-    (revert-buffer t t)))
+  (let ((coding-system-for-read coding-system)
+        (point (point)))
+    (revert-buffer t t)
+    (goto-char point)))
     ;(revert-buffer-no-confirm)))
 
+(defun revert-buffer-no-confirm ()
+  (interactive)
+  (let ((point (point)))
+    (revert-buffer t t)
+    (goto-char point)))
 
 (provide 'init-encoding)
