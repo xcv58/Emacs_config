4769ee684c2b8f0ffbcb8c794c965332b60667f8
xcv58 <chenyihonglove@gmail.com>
2014-04-26 04:22:40 -0400

    minor change on indent.

--- lisp/init-daemon.el
+++ lisp/init-daemon.el
@@ -2,9 +2,15 @@
 
 (unless (server-running-p)
   (server-start))
+
 (defun uni-frame()
-(unless (find 'ns (mapcar #'(lambda (x) (if t (framep x))) (frame-list))) (make-frame '((window-system . ns)))))
+  (unless
+      (find 'ns
+            (mapcar #'(lambda (x) (if t (framep x))) (frame-list)))
+    (make-frame '((window-system . ns)))))
+
 (defun exist-ns-frame()
-(find 'ns (mapcar #'(lambda (x) (if t (framep x))) (frame-list))))
+  (find 'ns
+        (mapcar #'(lambda (x) (if t (framep x))) (frame-list))))
 
 (provide 'init-daemon)
