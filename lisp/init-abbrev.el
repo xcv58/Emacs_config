3cbbdecca082852b3a63ec51818007c6fbedd9d5
xcv58 <chenyihonglove@gmail.com>
2014-05-27 01:44:19 -0400

    Use abbrev feature.

---
+++ lisp/init-abbrev.el
@@ -0,0 +1,17 @@
+(define-abbrev-table
+  'global-abbrev-table '(
+                         ("afaict" "as far as I can tell" nil 1)
+                         ("omuse" "http://www.emacswiki.org/cgi-bin/oddmuse.pl" nil 0)
+                         ("btw" "by the way" nil 3)
+                         ("wether" "whether" nil 5)
+                         ("ewiki" "http://www.emacswiki.org/cgi-bin/wiki.pl" nil 3)
+                         ("sop" "System.out.println")
+                         ))
+
+;; stop asking whether to save newly added abbrev when quitting emacs
+(setq save-abbrevs nil)
+
+;; turn on abbrev mode globally
+(setq-default abbrev-mode t)
+
+(provide 'init-abbrev)
