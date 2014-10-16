11f06b1f613a9501621490b3ecd0f91633202400
xcv58 <chenyihonglove@gmail.com>
2014-10-16 19:36:29 -0400

    temp turn off function eldoc-print-current-symbol-info for unknown error message.

--- init.el
+++ init.el
@@ -90,6 +90,9 @@
 ;;----------------------------------------------------------------------------
 (require 'init-daemon)
 
+(defun eldoc-print-current-symbol-info ()
+  ;; temp turn off this function.
+  )
 
 ;;; Show startup time
 (message "init completed in %.2fms" (sanityinc/time-subtract-millis (current-time) before-init-time))
