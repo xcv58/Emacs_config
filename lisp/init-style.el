fb25c5a365cdc4916775339d1fde15bfe645f52e
xcv58 <chenyihonglove@gmail.com>
2014-11-18 00:24:10 -0500

    ban tab indent.

--- lisp/init-style.el
+++ lisp/init-style.el
@@ -1,3 +1,7 @@
 (require-package 'google-c-style)
+(add-hook 'c-mode-common-hook 'google-set-c-style)
+(add-hook 'c-mode-common-hook 'google-make-newline-indent)
+
+(setq indent-tabs-mode nil)
 
 (provide 'init-style)
