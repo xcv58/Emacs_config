8a1b2c61a47f8567917b9fb94ef6568de860fdb7
xcv58 <chenyihonglove@gmail.com>
2014-11-18 10:01:37 -0500

    change style to k&r

--- lisp/init-style.el
+++ lisp/init-style.el
@@ -1,6 +1,8 @@
 (require-package 'google-c-style)
-(add-hook 'c-mode-hook 'google-set-c-style)
+;; (add-hook 'c-mode-hook 'google-set-c-style)
 
-(setq indent-tabs-mode nil)
+(setq c-default-style "k&r")
+(setq indent-tabs-mode nil
+      c-basic-offset 4)
 
 (provide 'init-style)
