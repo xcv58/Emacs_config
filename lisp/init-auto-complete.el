047c312fcf050cffa29b3497573464643e9d4aba
xcv58 <chenyihonglove@gmail.com>
2015-02-17 11:24:34 -0500

    add ycmd support.

--- lisp/init-auto-complete.el
+++ lisp/init-auto-complete.el
@@ -1,6 +1,9 @@
 (require-package 'auto-complete)
 ;; (require-package 'helm)
 ;; (require-package 'ac-helm)
+(require-package 'ycmd)
+(ycmd-setup)
+(set-variable 'ycmd-server-command '("python" "/Users/xcv58/.emacs.d/elpa/ycmd/ycmd"))
 
 (require 'auto-complete-config)
 (ac-config-default)
