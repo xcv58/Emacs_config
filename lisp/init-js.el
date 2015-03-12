0cae6fe1a216356dcbb34e05e8dfbbbdf9dd10e3
xcv58 <chenyihonglove@gmail.com>
2015-03-11 21:51:31 -0400

    let js2-mode be default for JavaScript

--- lisp/init-js.el
+++ lisp/init-js.el
@@ -1,8 +1,37 @@
 ;; (require-package 'js3-mode)
 (require-package 'js2-mode)
 
+(add-to-list 'auto-mode-alist (cons (rx ".js" eos) 'js2-mode))
+
 (setq js-indent-level 2)
 
-(add-hook 'js-mode-hook 'js2-minor-mode)
+;; (add-hook 'js-mode-hook 'js2-minor-mode)
+
+(eval-after-load "js2-mode"
+  '(progn
+     (setq js2-missing-semi-one-line-override t)
+     (setq-default
+      js2-global-externs
+      '("$"
+        "module"
+        "require"
+        "buster"
+        "sinon"
+        "assert"
+        "refute"
+        "setTimeout"
+        "clearTimeout"
+        "setInterval"
+        "json2html"
+        "hljs"
+        "clearInterval"
+        "location"
+        "__dirname"
+        "console"
+        "Meteor"
+        "Template"
+        "CollectionAPI"
+        "JSON"))
+     (setq-default js2-basic-offset 2)))
 
 (provide 'init-js)
