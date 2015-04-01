8ce425d2d9876d661d047214b952704d8a5f2d89
xcv58 <chenyihonglove@gmail.com>
2015-03-31 22:55:33 -0400

    seperate base.zsh for install.zsh

--- install.zsh
+++ install.zsh
@@ -1,26 +1,7 @@
 #!/bin/zsh
-TARGET="${HOME}/.emacs.d"
-
 SCRIPT_FILE=$0
 SCRIPT_PATH=$(dirname $SCRIPT_FILE)
+source ${SCRIPT_PATH}/base.zsh
 
-cd ${SCRIPT_PATH}
-dir=$(basename ${PWD})
-cd ..
-SOURCE="${PWD}/${dir}"
-
-if [[ -d ${TARGET} ]]; then
-    realpath=$(readlink ${TARGET})
-    if [[ ${realpath} == ${SOURCE} ]]; then
-        echo "Already install"
-        exit
-    else
-        echo "mv ${TARGET} ${TARGET}_BACK_`date +%s`"
-        eval "mv ${TARGET} ${TARGET}_BACK_`date +%s`"
-    fi
-fi
-
-link_command="ln -sf ${SOURCE} ${TARGET}"
-
-echo ${link_command}
-eval ${link_command}
+TARGET="${HOME}/.emacs.d"
+link_dir ${0} ${TARGET}
